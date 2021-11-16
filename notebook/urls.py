a61874be6b1596db1a32e572b7ef281df71efa8c
jeny797 <jeny797@gmail.com>
2021-11-16 16:52:02 +0200

    temp commit

--- notebook/urls.py
+++ notebook/urls.py
@@ -5,5 +5,6 @@ app_name = 'notebook'
 
 urlpatterns = [
     path('', views.ContactListView.as_view(), name='contact_list'),
-    path('<int:contact_id>/', views.contact_detail, name='contact_detail')
+    path('<slug:pk>/edit/', views.contact_edit,
+         name='contact_edit')
     ]
--- notebook/views.py
+++ notebook/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.shortcuts import HttpResponseRedirect, reverse
+from django.views.generic import ListView, UpdateView
 
 from .models import Contact
 
 
 # Create your views here.
 class ContactListView(ListView):
-
     queryset = Contact.objects.all()
     context_object_name = 'contacts'
     template_name = 'notebook/list.html'
     paginate_by = 15
 
 
-def contact_detail(request, contact_id):
+# class ContactEditView(UpdateView):
+#     model = Contact
+#     # fields = ['first_name', 'last_name', 'e_mail', 'phone_number', 'created']
+#     fields = '__all__'
+#     template_name = 'notebook/update.html'
+#
+#
+#
+#     def get_success_url(self):
+#
+#         return '%s?status_message=Contact is success updated' % reverse(
+#             'notebook:contact_list'
+#             )
+#
+#     def post(self, request, *args, **kwargs):
+#
+#         if request.POST.get('cancel_button'):
+#             return HttpResponseRedirect(
+#                 '%s?status_message=Update canceled' %
+#                 reverse('notebook:contact_list')
+#                 )
+#         else:
+#             return super().post(request, *args, **kwargs)
 
-    contact_detail = get_object_or_404(Contact, id=contact_id)
-    return render(request, 'notebook/detail.html')
+def contact_edit(request, pk):
+    print(Contact.objects.all())
+    contact = Contact.objects.get(id=int(pk))
+
+    return contact
